98ea170fdeefa269aa35bdadd46e83af5f4fd782
damienn7 <damien.nicolleau@epitech.eu>
2022-11-23 16:17:49 +0100

    Ajout de l'exercice 15

---
+++ ex_15.php
@@ -0,0 +1,19 @@
+<?php
+
+function teacher()
+{
+    echo "I am a Teacher";
+}
+
+function student(string $name)
+{
+    echo "I am a student and my name is $name";
+}
+
+
+
+$func_teacher='teacher';
+$func_student='student';
+
+$func_student('marco');
+$func_teacher();
