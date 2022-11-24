2e56d50fa343bc3d07662443442f5fb2ae9f0121
damienn7 <damien.nicolleau@epitech.eu>
2022-11-24 09:36:53 +0100

    Correction de l'exercice 19

--- ex_19.php
+++ ex_19.php
@@ -4,16 +4,13 @@ function calc(string $operation, int $nb1, int $nb2)
 {
     switch($operation):
         case "/":
-            if($nb1<$nb2) $result = $nb2 / $nb1;
-            else $result = $nb1 / $nb2;
+            $result = $nb1 / $nb2;
             break;
         case "%":
-            if($nb1<$nb2) $result = $nb2 % $nb1;
-            else $result = $nb1 % $nb2;
+            $result = $nb1 % $nb2;
             break;
         case "-":
-            if($nb1<$nb2) $result = $nb2 - $nb1;
-            else $result = $nb1 - $nb2;
+            $result = $nb1 - $nb2;
             break;
         case "+":
             $result = $nb1 + $nb2;
@@ -22,10 +19,9 @@ function calc(string $operation, int $nb1, int $nb2)
             $result = $nb1 * $nb2;
             break;
         default:
-            $result=false;
+            $result=0;
             break;
     endswitch;
-
 
     return $result;
 }
